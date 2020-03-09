67b18eba201bcc9d3d4bcdfff10ca799250ebeae
Shiladitya Biswas <shiladitya97@outlook.com>
2020-03-09 03:15:18 -0700

    Camera intrinsics Added

--- hw3_main.py
+++ hw3_main.py
@@ -3,11 +3,29 @@ import numpy as np
 from utils import *
 from scipy.linalg import expm
 
-def projection(v):
+def pi_func(v):
+	'''
+	projection function
+	'''
 	v_proj=np.divide(v,v[2])
 	return v_proj
 
+def J_pi_func(v):
+	'''
+	Jacobian of projection function
+	'''
+	a=np.array([[1,0,-v[0]/v[2],0],
+			  [0,1,-v[1]/v[2],0],
+			  [0,0,0,0],
+			  [0,0,-v[3]/v[2],1]])
+	J=a/v[2]
+	return J
+
+
 def hat_operator(v):
+	'''
+	Hat operator
+	'''
 	return np.array([[0,-v[2],v[1]],[v[2],0,-v[0]],[-v[1],v[0],0]])
 
 # Stereo camera calibration matrix M
@@ -18,6 +36,7 @@ if __name__ == '__main__':
 	filename = "./data/0027.npz"
 	t,features,linear_velocity,rotational_velocity,K,b,cam_T_imu = load_data(filename)
 
+	o_T_r=np.array([[0,-1,0,0],[0,0,-1,0],[1,0,0,0],[0,0,0,1]])
 	trajectory = np.zeros((4,4,np.size(t)))
 	imu_mu_t_t = np.identity(4)					# mean
 	imu_sigma_t_t = np.identity(6)				# covariance
@@ -47,16 +66,18 @@ if __name__ == '__main__':
 	# P_imu_coord= np.matmul(np.linalg.inv(cam_T_imu),P)
 #In[]
 	# (a) IMU Localization via EKF Prediction
-	tau = t[0,time] - t[0,time-1]
-	ut = np.vstack((linear_velocity[:,time].reshape(3,1),rotational_velocity[:,time].reshape(3,1)))
-	ut_hat=np.hstack((np.vstack((hat_operator(rotational_velocity[:,time]),np.zeros(3))), np.hstack((linear_velocity[:,time],0)).reshape(4,1) ))
-	ut_cap=np.vstack((np.hstack((hat_operator(rotational_velocity[:,time]),hat_operator(linear_velocity[:,time]))),np.hstack((np.zeros((3,3)), hat_operator(rotational_velocity[:,time]))) ))
-	imu_mu_t_t=np.matmul(expm(-tau*ut_hat),imu_mu_t_t)
-	imu_sigma_t_t=expm(-tau*ut_cap)@imu_sigma_t_t@np.transpose(expm(-tau*ut_cap))+np.random.multivariate_normal(np.zeros(6),0.5*np.identity(6),6)
-	trajectory[:,:,time] = np.linalg.inv(imu_mu_t_t)
-	# (b) Landmark Mapping via EKF Update
-	W_T_Cam = np.linalg.inv(np.matmul(cam_T_imu,imu_mu_t_t))
-
+	for time in np.arange(1,2,1):
+		tau = t[0,time] - t[0,time-1]
+		ut = np.vstack((linear_velocity[:,time].reshape(3,1),rotational_velocity[:,time].reshape(3,1)))
+		ut_hat=np.hstack((np.vstack((hat_operator(rotational_velocity[:,time]),np.zeros(3))), np.hstack((linear_velocity[:,time],0)).reshape(4,1) ))
+		ut_cap=np.vstack((np.hstack((hat_operator(rotational_velocity[:,time]),hat_operator(linear_velocity[:,time]))),np.hstack((np.zeros((3,3)), hat_operator(rotational_velocity[:,time]))) ))
+		imu_mu_t_t=np.matmul(expm(-tau*ut_hat),imu_mu_t_t)
+		imu_sigma_t_t=expm(-tau*ut_cap)@imu_sigma_t_t@np.transpose(expm(-tau*ut_cap))+np.random.multivariate_normal(np.zeros(6),0.5*np.identity(6),6)
+		trajectory[:,:,time] = np.linalg.inv(imu_mu_t_t)
+		# (b) Landmark Mapping via EKF Update
+#In[]
+	Cam_T_W=np.matmul(cam_T_imu,imu_mu_t_t)
+	W_T_Cam = np.linalg.inv(Cam_T_W)
 	ind=np.array(np.where(np.sum(features[:,:,time],axis=0)!=-4 ))[0]
 	x_by_z=(features[0,ind,time]-M[0,2])/M[0,0]
 	y_by_z=(features[1,ind,time]-M[1,2])/M[1,1]
@@ -64,19 +85,26 @@ if __name__ == '__main__':
 	x=x_by_z/one_by_z
 	y=y_by_z/one_by_z
 	z=1/one_by_z
-	P=np.vstack((x,y,z,np.ones(len(ind))))
+	# P_optical=np.linalg.pinv(M)@features[:,:,time]
+	P_optical=np.vstack((x,y,z,np.ones(len(ind))))
+	P_cam=np.matmul(np.linalg.inv(o_T_r),P_optical)
+	# Convert to world frame
+	P_world_coord= np.matmul(W_T_Cam,P_cam)
+#In[]
+		for j in range(len(ind)):
+			if (np.array_equal(landmark_mu_t[:,ind[j]], np.array([-1,-1,-1,-1]) )):
+				landmark_mu_t[:,ind[j]]=P_world_coord[:,j]
+			else:
+				
 
-	# Convert to imu coord
-	P_world_coord= np.matmul(np.linalg.inv(W_T_Cam),P)
-	for j in ind:
-		pass
+
 
 
 	# (c) Visual-Inertial SLAM
-
+#In[]
 	# You can use the function below to visualize the robot pose over time
 	#visualize_trajectory_2d(world_T_imu,show_ori=True)
-	# visualize_trajectory_2d(trajectory,show_ori=True)
+	visualize_trajectory_2d(trajectory,show_ori=True)
 
 
 # %%
