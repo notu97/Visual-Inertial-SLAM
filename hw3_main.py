a1ad866b90752b604d252ac3ce8eb95e15316892
Shiladitya Biswas <shiladitya97@outlook.com>
2020-03-09 18:12:00 -0700

    commit

--- hw3_main.py
+++ hw3_main.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 	imu_mu_t_t = np.identity(4)					# mean
 	imu_sigma_t_t = np.identity(6)				# covariance
 	trajectory[:,:,0] = imu_mu_t_t
+	Proj=np.block([np.identity(3),np.zeros((3,1))])
 
 	M=np.block([[K[0:2,:], np.zeros((2,1))],[K[0:2,:], np.zeros((2,1))]])
 	M[2,3] = -K[0,0]*b
@@ -79,23 +80,45 @@ if __name__ == '__main__':
 	Cam_T_W=np.matmul(cam_T_imu,imu_mu_t_t)
 	W_T_Cam = np.linalg.inv(Cam_T_W)
 	ind=np.array(np.where(np.sum(features[:,:,time],axis=0)!=-4 ))[0]
-	x_by_z=(features[0,ind,time]-M[0,2])/M[0,0]
-	y_by_z=(features[1,ind,time]-M[1,2])/M[1,1]
-	one_by_z=(-features[2,ind,time]+M[2,2]+(M[2,0]*x_by_z))/M[2,3]
-	x=x_by_z/one_by_z
-	y=y_by_z/one_by_z
-	z=1/one_by_z
-	# P_optical=np.linalg.pinv(M)@features[:,:,time]
-	P_optical=np.vstack((x,y,z,np.ones(len(ind))))
-	P_cam=np.matmul(np.linalg.inv(o_T_r),P_optical)
-	# Convert to world frame
-	P_world_coord= np.matmul(W_T_Cam,P_cam)
+	# x_by_z=(features[0,ind,time]-M[0,2])/M[0,0]
+	# y_by_z=(features[1,ind,time]-M[1,2])/M[1,1]
+	# one_by_z=(-features[2,ind,time]+M[2,2]+(M[2,0]*x_by_z))/M[2,3]
+	# x=x_by_z/one_by_z
+	# y=y_by_z/one_by_z
+	# z=1/one_by_z
+	# # P_optical=np.linalg.pinv(M)@features[:,:,time]
+	# P_optical=np.vstack((x,y,z,np.ones(len(ind))))
+	# P_cam=np.matmul(np.linalg.inv(o_T_r),P_optical)
+	# # Convert to world frame
+	# P_world_coord= np.matmul(W_T_Cam,P_cam)
 #In[]
+	if (len(ind.T)!=0):
+		valid_features=features[:,ind,time]
+		x_by_z=(valid_features[0,:]-M[0,2])/M[0,0]
+		y_by_z=(valid_features[1,:]-M[1,2])/M[1,1]
+		one_by_z=(-valid_features[2,:]+M[2,2]+(M[2,0]*x_by_z))/M[2,3]
+		x=x_by_z/one_by_z
+		y=y_by_z/one_by_z
+		z=1/one_by_z
+		# P_optical=np.linalg.pinv(M)@features[:,:,time]
+		P_optical=np.vstack((x,y,z,np.ones(len(ind))))
+		P_cam=np.matmul(np.linalg.inv(o_T_r),P_optical)
+		# Convert to world frame
+		P_world_coord= np.matmul(W_T_Cam,P_cam)
+		update_feature_ind=np.empty(0)
+		update_feature=np.zeros((4,1))
+
+# In[]	
 		for j in range(len(ind)):
+			curr_ind=ind[j]
 			if (np.array_equal(landmark_mu_t[:,ind[j]], np.array([-1,-1,-1,-1]) )):
 				landmark_mu_t[:,ind[j]]=P_world_coord[:,j]
 			else:
-				
+				# get the index of landmarks whose mu and sigma is to be updated
+				update_feature_ind=np.append(update_feature_ind,curr_ind) 
+				update_feature=np.hstack((update_feature,P_world_coord[:,j].reshape(4,1)))
+
+		if(np.size(update_feature_ind)!=0):
 
 
 
