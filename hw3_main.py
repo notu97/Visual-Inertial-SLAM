1fb4af2c4223b7caf4f2a2a1f6e2362f0060e45b
Shiladitya Biswas <shiladitya97@outlook.com>
2020-03-10 02:08:57 -0700

    matmul chnages required

--- hw3_main.py
+++ hw3_main.py
@@ -46,9 +46,9 @@ if __name__ == '__main__':
 
 	M=np.block([[K[0:2,:], np.zeros((2,1))],[K[0:2,:], np.zeros((2,1))]])
 	M[2,3] = -K[0,0]*b
-	V=10
+	V=100
 	landmark_mu_t = -1*np.ones((4,np.shape(features)[1]))	# mean	4*M
-	landmark_sigma_t = np.identity(3*np.shape(features)[1])*10	# covariance	3M*3M
+	landmark_sigma_t = np.identity(3*np.shape(features)[1])*V	# covariance	3M*3M
 	I = np.vstack((np.identity(3),np.zeros((1,3))))
 	I = np.kron(np.eye(np.shape(features)[1]),I)
 #In[]	
@@ -69,6 +69,7 @@ if __name__ == '__main__':
 #In[]
 	# (a) IMU Localization via EKF Prediction
 	for time in np.arange(1,len(t.T),1):
+		print(time-1)
 		tau = t[0,time] - t[0,time-1]
 		ut = np.vstack((linear_velocity[:,time].reshape(3,1),rotational_velocity[:,time].reshape(3,1)))
 		ut_hat=np.hstack((np.vstack((hat_operator(rotational_velocity[:,time]),np.zeros(3))), np.hstack((linear_velocity[:,time],0)).reshape(4,1) ))
@@ -81,6 +82,8 @@ if __name__ == '__main__':
 		Cam_T_W=np.matmul(cam_T_imu,imu_mu_t_t)
 		W_T_Cam = np.linalg.inv(Cam_T_W)
 		ind=np.array(np.where(np.sum(features[:,:,time],axis=0)!=-4 ))[0]
+		update_feature_ind=np.empty(0,np.int16)
+		update_feature=np.empty((4,0))
 		# x_by_z=(features[0,ind,time]-M[0,2])/M[0,0]
 		# y_by_z=(features[1,ind,time]-M[1,2])/M[1,1]
 		# one_by_z=(-features[2,ind,time]+M[2,2]+(M[2,0]*x_by_z))/M[2,3]
@@ -95,6 +98,7 @@ if __name__ == '__main__':
 		#In[]
 		if (len(ind.T)!=0):
 			valid_features=features[:,ind,time]
+			features_time=features[:,:,time]
 			x_by_z=(valid_features[0,:]-M[0,2])/M[0,0]
 			y_by_z=(valid_features[1,:]-M[1,2])/M[1,1]
 			one_by_z=(-valid_features[2,:]+M[2,2]+(M[2,0]*x_by_z))/M[2,3]
@@ -106,8 +110,7 @@ if __name__ == '__main__':
 			P_cam=np.matmul(np.linalg.inv(o_T_r),P_optical)
 			# Convert to world frame
 			P_world_coord= np.matmul(W_T_Cam,P_cam)
-			update_feature_ind=np.empty(0,np.int16)
-			update_feature=np.zeros((4,1))
+			
 
 		# In[]	
 			for j in range(len(ind)):
@@ -121,8 +124,8 @@ if __name__ == '__main__':
 
 			if(np.size(update_feature_ind)!=0):
 				mu_t_bar=landmark_mu_t[:,update_feature_ind]
-				z_t_bar=M@pi_func(o_T_r@Cam_T_W@mu_t_bar)
-				z_t=valid_features[:,update_feature_ind]
+				z_t_bar=M@pi_func(np.linalg.multi_dot ([o_T_r,Cam_T_W,mu_t_bar])) 
+				z_t=features_time[:,update_feature_ind]
 				temp=Cam_T_W@mu_t_bar
 				H_t=np.zeros((4*(len(temp.T)),3*(np.shape(features)[1]) ))
 				for i in range(len(temp)):
@@ -142,7 +145,7 @@ if __name__ == '__main__':
 	visualize_trajectory_2d(trajectory,landmark_mu_t,path_name="Unknown",show_ori=True)
 
 
-# %%
+# %% 98
 
 
 # %%
